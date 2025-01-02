defefe5f1980488629c365ea39632c91b6dae73d
Jamesthe1 <jamesthe1sky@gmail.com>
2025-01-01 23:39:46 -0600

    Prepare for xdelta DLL

---
+++ libs/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -d xdelta/build ]; then mkdir xdelta/build; fi
+cd xdelta/build
+cmake -DBUILD_SHARED_LIBS=ON -DXD3_ENABLE_SECONDARY_COMPRESSION=ON -DXD3_ENABLE_ENCODER=ON -DXD3_ENABLE_VCDIFF_TOOLS=ON -DXD3_ENABLE_LZMA=ON -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ..
+make -j
+cd -
