fc00e07910dcb569db13fa9fe3574e4f1f832339
Jamesthe1 <jamesthe1sky@gmail.com>
2025-01-09 16:49:59 -0600

    Move built dll to lib folder

--- libs/build.sh
+++ libs/build.sh
@@ -4,4 +4,5 @@ if [ ! -d xdelta/build ]; then mkdir xdelta/build; fi
 cd xdelta/build
 cmake -DBUILD_SHARED_LIBS=ON -DXD3_ENABLE_SECONDARY_COMPRESSION=ON -DXD3_ENABLE_ENCODER=ON -DXD3_ENABLE_VCDIFF_TOOLS=ON -DXD3_ENABLE_LZMA=ON -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ..
 make -j
+mv libxdelta3.dll ../../libxdelta3.dll
 cd -
