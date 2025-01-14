efe82b4f1dd659a77428e9f7b22b41d745017048
Jamesthe1 <jamesthe1sky@gmail.com>
2025-01-14 17:03:12 -0600

    Add multiple build targets

--- libs/build.sh
+++ libs/build.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-if [ ! -d xdelta/build ]; then mkdir xdelta/build; fi
-cd xdelta/build
-cmake -DBUILD_SHARED_LIBS=ON -DXD3_ENABLE_SECONDARY_COMPRESSION=ON -DXD3_ENABLE_ENCODER=ON -DXD3_ENABLE_VCDIFF_TOOLS=ON -DXD3_ENABLE_LZMA=ON -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ..
-make -j
-mv libxdelta3.dll ../../../libxdelta3.dll
-cd -
+case $1 in
+    xz)
+        cd xz/windows
+        if [ -d build ]; then rm -r build; fi
+        CMAKE_DIR=$(which cmake)
+        GCC_DIR=$(which gcc)
+        ./build_with_cmake.bat $(dirname CMAKE_DIR) $(dirname CMAKE_DIR) ON
+        mv build/liblzma.dll ../../..
+        cd -
+        ;;
+    xdelta)
+        if [ ! -d xdelta/build ]; then mkdir xdelta/build; fi
+        cd xdelta/build
+        cmake -DBUILD_SHARED_LIBS=ON -DXD3_ENABLE_SECONDARY_COMPRESSION=ON -DXD3_ENABLE_ENCODER=ON -DXD3_ENABLE_VCDIFF_TOOLS=ON -DXD3_ENABLE_LZMA=ON -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ..
+        make -j
+        mv libxdelta3.dll ../../..
+        cd -
+        ;;
+    bridge)
+        cd xdelta-bridge
+        make
+        cd -
+        ;;
+esac
